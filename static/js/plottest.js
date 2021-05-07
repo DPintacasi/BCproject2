118b96cc4a6dfb666e239938c7c7e170c20d5604
Ashley Ross <ashleyross@Ashleys-MacBook-Air.local>
2021-05-07 18:15:41 -0400

    ar

--- static/js/plottest.js
+++ static/js/plottest.js
@@ -4,20 +4,30 @@ d3.json("/data/redfin").then(function(data){
   var redfindata = data
   console.log(redfindata)
 
-
-  var total_homes_sold = redfindata.total_homes_sold;
-  var year = redfindata.year;
-  var year = redfindata.median_house_price;
-  // var startDate = data.dataset.start_date;
-  // var endDate = data.dataset.end_date;
-  // var dates = unpack(data.dataset.data, 0);
-  // var closingPrices = unpack(data.dataset.data, 4);
+  // function buildMetadata(sample) {
+  //   d3.json("samples.json").then((data) => {
+  //     var metadata = data.metadata;
+  //     // Filter the data for the object with the desired sample number
+  //     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  //     var result = resultArray[0];
+  //     // Use d3 to select the panel with id of `#sample-metadata`
+  //     var PANEL = d3.select("#sample-metadata");
+  
+  //     // Use `.html("") to clear any existing metadata
+  //     PANEL.html("");
+  
+  //     // Use `Object.entries` to add each key and value pair to the panel
+  //     // Hint: Inside the loop, you will need to use d3 to append new
+  //     // tags for each key-value in the metadata.
+  //     Object.entries(result).forEach(([key, value]) => {
+  //       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  //     });
+  
 
 // Trace1 for the homeownership data
 var trace1 = {
     x: redfindata.map(row => row.total_homes_sold),
     y: redfindata.map(row => row.year),
-    // text: data.map(row => row.date),
     name: "Homweownership Rate",
     type: "bar"
   };
